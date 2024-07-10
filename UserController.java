fefb18c80a6c07df350c77366221f2cec99aa450
michaelrobust <ray4081161@icloud.com>
2024-07-10 11:25:08 +0800

    Create UserController.java

---
+++ UserController.java
@@ -0,0 +1,24 @@
+package com.example.restaurant.controller;
+
+import com.example.restaurant.model.User;
+import com.example.restaurant.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@RequestBody User user) {
+        return userService.loginUser(user);
+    }
+}
